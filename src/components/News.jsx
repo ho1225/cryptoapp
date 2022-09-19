ef1b4eb0cd33bb967dd2fa9edaf3da7a7cff0e51
ho1225 <56968144+ho1225@users.noreply.github.com>
2022-09-19 23:19:52 +0800

    news update

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -7,15 +7,37 @@ import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 const { Text, Title } = Typography;
 const { Option } = Select;
 
-const News = ( simplified ) => {
-  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 10 : 100})
+const demoImage = 'http://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg'
 
-  console.log(cryptoNews)
+const News = ( simplified ) => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12})
+
+  if(!cryptoNews?.value) return 'Loading...';
   
   return (
-    <div>
-      News
-    </div>
+    <Row gutter={[ 24, 24 ]}>
+        {cryptoNews.value.map((news, i) => (
+            <Col xs={24} sm={12} lg={8} key={i}>
+                <Card hoverable className="news-card">
+                    <a href={news.url} target="_blank" rel="noreferrer">
+                        <div className='news-image-container'>
+                            <Title className="new-title" level={4}>{news.name}</Title>
+                            <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt="news" />
+                        </div>
+                        <p>
+                            {news.description > 100 ? `${news.description.substring(0, 100)} ... ` : news.description}
+                        </p>
+                        <div className="provider-container">
+                            <div>
+                                <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="news" />
+                                <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
+                            </div>
+                        </div>
+                    </a>
+                </Card>
+            </Col>
+        ))}
+    </Row>
   )
 }
 
