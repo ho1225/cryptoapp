c185628fd93c44707db1e8d0dc2e57188656dce8
ho1225 <56968144+ho1225@users.noreply.github.com>
2022-09-27 00:55:26 +0800

    API correction

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import HTMLReactParser from 'html-react-parser';
+import { useParams } from 'react-router-dom';
+import millify from 'millify';
+import { Col, Row, Typography, Select } from 'antd';
+import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
+
+import { useGetCryptoDetailsQuery, useGetCryptosQuery } from '../services/cryptoApi';
+
+
+const { Title, Text } = Typography;
+const { Option } = Select;
 
 const CryptoDetails = () => {
+  const { coinId } = useParams();
+  const { data: cryptosList } = useGetCryptosQuery(100);
+  const { timePeriod, setTimePeriod } = useState('7d');
+  
+  const { data, isFetching } = useGetCryptoDetailsQuery(cryptosList ? cryptosList.data.coins[coinId-1].uuid : coinId);
+
+  console.log(data);
+
   return (
     <div>
-      CryptoDetails
+      CryptoDetails {coinId}
     </div>
   )
 }
